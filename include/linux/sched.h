bcefc35a830dee2b632bae56cc06194d12381cf7
Chris Redpath <chris.redpath@arm.com>
2013-05-16 17:48:24 +0100

    sched: Ignore offline CPUs in HMP migration & load stats
    Previously, an offline CPU would always appear to have a zero load
    and this would distort the offload functionality used for balancing
    big and little domains.
    
    Maintain a mask of online CPUs in each domain and use this instead.
    
    Change-Id: I30b1dd60199a64d48397e6a1515f72bcbb540784
    Signed-off-by: Chris Redpath <chris.redpath@arm.com>
    Signed-off-by: Seokju Yoon <sukju.yoon@samsung.com>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1038,6 +1038,7 @@ extern void wake_up_if_idle(int cpu);
 #ifdef CONFIG_SCHED_HMP
 struct hmp_domain {
 	struct cpumask cpus;
+	struct cpumask possible_cpus;
 	struct list_head hmp_domains;
 };
 #endif /* CONFIG_SCHED_HMP */
