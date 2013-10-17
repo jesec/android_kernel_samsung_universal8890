7d99782c236837f45fff4bc96d6f4fd917d3ec73
Hwang Seungkon <skon.hwang@samsung.com>
2013-10-17 10:57:00 +0900

    sched: Enable hmp_boost while booting regardless of HMP_FREQUENCY_INVARIANT_SCALE
    Change-Id: I82deb4d9f065e02d757d15bc212984fe77ad1738
    Signed-off-by: Hwang Seungkon <skon.hwang@samsung.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -9472,6 +9472,8 @@ static int __init register_sched_cpufreq_notifier(void)
 
 core_initcall(register_sched_cpufreq_notifier);
 
+#endif /* CONFIG_HMP_FREQUENCY_INVARIANT_SCALE */
+
 #if BOOT_BOOST_DURATION
 static int __init hmp_boot_boost(void)
 {
@@ -9481,5 +9483,3 @@ static int __init hmp_boot_boost(void)
 }
 pure_initcall(hmp_boot_boost);
 #endif
-
-#endif /* CONFIG_HMP_FREQUENCY_INVARIANT_SCALE */
