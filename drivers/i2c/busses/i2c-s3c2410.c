3121fc8c0b6e065cd965318be7ba28cbeb639e09
Hyeonkook Kim <hk619.kim@samsung.com>
2013-12-11 13:26:43 +0900

    i2c: s3c2410: remove S3C24XX I2C frequency scaling
    Thia patch removes CPU frequency scaling to the S3C24XX I2C driver.
    The S3C24XX I2C driver doesn't depends on CPU frequency scaling.
    
    Signed-off-by: Hyeonkook Kim <hk619.kim@samsung.com>
    
    Conflicts:
    
    	drivers/i2c/busses/i2c-s3c2410.c
    
    Change-Id: If9fa067fd54ea3da2ec459f99db24002c8b7b3d2

--- drivers/i2c/busses/i2c-s3c2410.c
+++ drivers/i2c/busses/i2c-s3c2410.c
@@ -29,7 +29,6 @@
 #include <linux/platform_device.h>
 #include <linux/pm_runtime.h>
 #include <linux/clk.h>
-#include <linux/cpufreq.h>
 #include <linux/slab.h>
 #include <linux/io.h>
 #include <linux/of.h>
@@ -1279,13 +1278,6 @@ static int s3c24xx_i2c_probe(struct platform_device *pdev)
 		}
 	}
 
-	ret = s3c24xx_i2c_register_cpufreq(i2c);
-	if (ret < 0) {
-		dev_err(&pdev->dev, "failed to register cpufreq notifier\n");
-		clk_unprepare(i2c->clk);
-		return ret;
-	}
-
 	/* Note, previous versions of the driver used i2c_add_adapter()
 	 * to add the bus at any number. We now pass the bus number via
 	 * the platform data, so if unset it will now default to always
@@ -1298,8 +1290,6 @@ static int s3c24xx_i2c_probe(struct platform_device *pdev)
 	ret = i2c_add_numbered_adapter(&i2c->adap);
 	if (ret < 0) {
 		dev_err(&pdev->dev, "failed to add bus to i2c core\n");
-		s3c24xx_i2c_deregister_cpufreq(i2c);
-		clk_unprepare(i2c->clk);
 		return ret;
 	}
 
@@ -1325,8 +1315,6 @@ static int s3c24xx_i2c_remove(struct platform_device *pdev)
 
 	pm_runtime_disable(&i2c->adap.dev);
 	pm_runtime_disable(&pdev->dev);
-
-	s3c24xx_i2c_deregister_cpufreq(i2c);
 
 	i2c_del_adapter(&i2c->adap);
 
