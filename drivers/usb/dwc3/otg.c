abdf7570a1a54825296770d0627c40f2b65622b5
Minho Lee <minho@ishtar.(none)>
2014-02-07 16:16:36 +0900

    [COMMON] usb: dwc3: Change delay logic for disconnection
    To process USB disconnect interrupt, wait until USB disconnect IRQ done.
    
    Change-Id: Iae2f93ee803d461f2364e2b18cfcc60f6c4000e3
    Signed-off-by: Anton Tikhomirov <av.tikhomirov@samsung.com>
    Signed-off-by: Minho Lee <minho@ishtar.(none)>

--- drivers/usb/dwc3/otg.c
+++ drivers/usb/dwc3/otg.c
@@ -259,6 +259,7 @@ static int dwc3_otg_start_gadget(struct otg_fsm *fsm, int on)
 			goto err2;
 		}
 	} else {
+		msleep(200);
 		ret = usb_gadget_vbus_disconnect(otg->gadget);
 		if (ret)
 			dev_err(dwc->dev, "%s: vbus disconnect failed\n",
