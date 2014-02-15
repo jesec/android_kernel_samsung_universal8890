d044c1698af3ed00f4facb638f1a4ed9a4e575b8
Changhwan Youn <chaos.yon@samsung.com>
2014-02-15 16:14:19 +0900

    Revert "HMP: Force new non-kernel tasks onto big CPUs until load stabilises"
    This reverts commit 70338394b118e5f8ecec1def58db1390193dc607.
    
    Change-Id: I1bc6cfcb756cbd29eb09e12335c18d53176bada4
    Signed-off-by: Seokju Yoon <sukju.yoon@samsung.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -1844,18 +1844,8 @@ static void __sched_fork(unsigned long clone_flags, struct task_struct *p)
 	p->se.avg.runnable_avg_sum = 0;
 	p->se.avg.remainder = 0;
 #ifdef CONFIG_SCHED_HMP
-	/* keep LOAD_AVG_MAX in sync with fair.c if load avg series is changed */
-#define LOAD_AVG_MAX 47742
-	if (!p->parent || p->parent->pid > 2) {
-		p->se.avg.hmp_last_up_migration = 0;
-		p->se.avg.hmp_last_down_migration = 0;
-		p->se.avg.load_avg_ratio = 1023;
-		p->se.avg.load_avg_contrib =
-				1023 * scale_load_down(p->se.load.weight);
-		p->se.avg.runnable_avg_period = LOAD_AVG_MAX;
-		p->se.avg.runnable_avg_sum = LOAD_AVG_MAX;
-		p->se.avg.usage_avg_sum = LOAD_AVG_MAX;
-	}
+	p->se.avg.hmp_last_up_migration = 0;
+	p->se.avg.hmp_last_down_migration = 0;
 #endif
 #endif
 #ifdef CONFIG_SCHEDSTATS
