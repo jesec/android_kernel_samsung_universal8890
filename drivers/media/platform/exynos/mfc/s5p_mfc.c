fa67ae622d9a7420a961ed6e8cef688bb848ac28
CHOIHYOJOO <hyoj.choi@samsung.com>
2014-05-02 20:30:07 +0900

    [COMMON] media: mfc: add qos table for supporting uhd contents
    Change-Id: Ibb89a7e20f7fde570ef9b5ca3b25b84537948ec3
    Signed-off-by: CHOIHYOJOO <hyoj.choi@samsung.com>

--- drivers/media/platform/exynos/mfc/s5p_mfc.c
+++ drivers/media/platform/exynos/mfc/s5p_mfc.c
@@ -2325,7 +2325,7 @@ static void *mfc_get_drv_data(struct platform_device *pdev);
 #ifdef CONFIG_MFC_USE_BUS_DEVFREQ
 #if defined(CONFIG_SOC_EXYNOS5430_REV_1)
 #define QOS_STEP_NUM (6)
-#elif defined(CONFIG_SOC_EXYNOS5422_REV_0)
+#elif defined(CONFIG_SOC_EXYNOS5422_REV_0) || defined(CONFIG_SOC_EXYNOS5433)
 #define QOS_STEP_NUM (5)
 #else
 #define QOS_STEP_NUM (4)
@@ -2411,7 +2411,9 @@ static void mfc_parse_dt(struct device_node *np, struct s5p_mfc_dev *mfc)
 	parse_mfc_qos_platdata(np, "mfc_qos_variant_1", &g_mfc_qos_table[1]);
 	parse_mfc_qos_platdata(np, "mfc_qos_variant_2", &g_mfc_qos_table[2]);
 	parse_mfc_qos_platdata(np, "mfc_qos_variant_3", &g_mfc_qos_table[3]);
-#if defined(CONFIG_SOC_EXYNOS5430_REV_1) || defined(CONFIG_SOC_EXYNOS5422_REV_0)
+#if defined(CONFIG_SOC_EXYNOS5430_REV_1) ||	\
+	defined(CONFIG_SOC_EXYNOS5422_REV_0) ||	\
+	defined(CONFIG_SOC_EXYNOS5433)
 	parse_mfc_qos_platdata(np, "mfc_qos_variant_4", &g_mfc_qos_table[4]);
 #if defined(CONFIG_SOC_EXYNOS5430_REV_1)
 	/* Max table for Decoder */
