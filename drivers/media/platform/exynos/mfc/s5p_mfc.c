dc7018e292a18fbaabb13bf7c7d01ed76f21402b
Hyeonung Kim <khw0178.kim@samsung.com>
2014-06-15 18:18:28 +0900

    [COMMON] media: mfc: fix incorrect handling for H.264 DPB
    Change-Id: Iead2c9faf4af00a7a28fb048ff34f4618ba731d1
    Signed-off-by: Janghyuck Kim <janghyuck.kim@samsung.com>

--- drivers/media/platform/exynos/mfc/s5p_mfc.c
+++ drivers/media/platform/exynos/mfc/s5p_mfc.c
@@ -1067,19 +1067,21 @@ static void s5p_mfc_handle_ref_frame(struct s5p_mfc_ctx *ctx)
 		list_add_tail(&dec_buf->list, &dec->ref_queue);
 		dec->ref_queue_cnt++;
 	} else if (is_h264(ctx)) {
-		int found = 0, temp_index;
+		int found = 0;
 
 		/* Try to search decoded address in whole dst queue */
 		list_for_each_entry(dec_buf, &ctx->dst_queue, list) {
-			temp_index = dec_buf->vb.v4l2_buf.index;
-			if ((dec->dynamic_used & (1 << temp_index)) == 0) {
+			buf_addr = s5p_mfc_mem_plane_addr(ctx, &dec_buf->vb, 0);
+			if ((buf_addr == dec_addr) && (dec_buf->used == 1)) {
 				found = 1;
 				break;
 			}
 		}
 
 		if (found) {
-			mfc_debug(2, "Dec buffer y in dst queue = 0x%x\n", dec_addr);
+			buf_addr = s5p_mfc_mem_plane_addr(ctx, &dec_buf->vb, 0);
+			mfc_debug(2, "Found in dst queue = 0x%x, buf = 0x%x\n",
+							dec_addr, buf_addr);
 
 			list_del(&dec_buf->list);
 			ctx->dst_queue_cnt--;
