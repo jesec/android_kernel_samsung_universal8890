16aa06384c8065f5ed6af1b73fbdc62a595ccd33
Anton Tikhomirov <av.tikhomirov@samsung.com>
2014-11-26 18:41:11 +0900

    [COMMON] usb: dwc3: core: Avoid cleaning event buffer address
    According to DWC3 databook software must initialize event buffer
    address once during power-on initialization and must not change
    the value of GEVNTADR register after it is initialized. If the
    address is cleared, this leads to NOC-Bus Error when DWC3
    controller tries to write to physical address 0.
    
    Change-Id: Ia4bf0821698e58f1d4f89c2362e3522ce883c7d2
    Signed-off-by: Anton Tikhomirov <av.tikhomirov@samsung.com>

--- drivers/usb/dwc3/core.c
+++ drivers/usb/dwc3/core.c
@@ -354,8 +354,6 @@ void dwc3_event_buffers_cleanup(struct dwc3 *dwc)
 
 		evt->lpos = 0;
 
-		dwc3_writel(dwc->regs, DWC3_GEVNTADRLO(n), 0);
-		dwc3_writel(dwc->regs, DWC3_GEVNTADRHI(n), 0);
 		dwc3_writel(dwc->regs, DWC3_GEVNTSIZ(n), DWC3_GEVNTSIZ_INTMASK
 				| DWC3_GEVNTSIZ_SIZE(0));
 		dwc3_writel(dwc->regs, DWC3_GEVNTCOUNT(n), 0);
