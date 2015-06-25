9e0c5b7d946bb7abdb2ee1cef9f4f94ee057d135
Kangnam Park <kn0710.park@samsung.com>
2015-06-25 10:38:04 +0900

    [COMMON] media: modified the prefix of video node
    Change-Id: Ibfa1926a0d909720b25f16630928bd1b736f7e43
    Signed-off-by: Kangnam Park <kn0710.park@samsung.com>

--- drivers/media/platform/exynos/mfc/s5p_mfc.c
+++ drivers/media/platform/exynos/mfc/s5p_mfc.c
@@ -2869,7 +2869,7 @@ static int s5p_mfc_probe(struct platform_device *pdev)
 
 	snprintf(vfd->name, sizeof(vfd->name), "%s%d", s5p_mfc_dec_videodev.name, dev->id);
 
-	ret = video_register_device(vfd, VFL_TYPE_GRABBER, S5P_VIDEONODE_MFC_DEC + 60*dev->id);
+	ret = video_register_device(vfd, VFL_TYPE_GRABBER, EXYNOS_VIDEONODE_MFC_DEC + 60*dev->id);
 	if (ret) {
 		v4l2_err(&dev->v4l2_dev, "Failed to register video device\n");
 		video_device_release(vfd);
@@ -2897,7 +2897,7 @@ static int s5p_mfc_probe(struct platform_device *pdev)
 	vfd->vfl_dir = VFL_DIR_M2M;
 	snprintf(vfd->name, sizeof(vfd->name), "%s%d", s5p_mfc_enc_videodev.name, dev->id);
 
-	ret = video_register_device(vfd, VFL_TYPE_GRABBER, S5P_VIDEONODE_MFC_ENC + 60*dev->id);
+	ret = video_register_device(vfd, VFL_TYPE_GRABBER, EXYNOS_VIDEONODE_MFC_ENC + 60*dev->id);
 	if (ret) {
 		v4l2_err(&dev->v4l2_dev, "Failed to register video device\n");
 		video_device_release(vfd);
@@ -2926,7 +2926,7 @@ static int s5p_mfc_probe(struct platform_device *pdev)
 
 	snprintf(vfd->name, sizeof(vfd->name), "%s%d", s5p_mfc_dec_drm_videodev.name, dev->id);
 
-	ret = video_register_device(vfd, VFL_TYPE_GRABBER, S5P_VIDEONODE_MFC_DEC_DRM + 60*dev->id);
+	ret = video_register_device(vfd, VFL_TYPE_GRABBER, EXYNOS_VIDEONODE_MFC_DEC_DRM + 60*dev->id);
 	if (ret) {
 		v4l2_err(&dev->v4l2_dev, "Failed to register video device\n");
 		video_device_release(vfd);
@@ -2954,7 +2954,7 @@ static int s5p_mfc_probe(struct platform_device *pdev)
 	vfd->vfl_dir = VFL_DIR_M2M;
 	snprintf(vfd->name, sizeof(vfd->name), "%s%d", s5p_mfc_enc_drm_videodev.name, dev->id);
 
-	ret = video_register_device(vfd, VFL_TYPE_GRABBER, S5P_VIDEONODE_MFC_ENC_DRM + 60*dev->id);
+	ret = video_register_device(vfd, VFL_TYPE_GRABBER, EXYNOS_VIDEONODE_MFC_ENC_DRM + 60*dev->id);
 	if (ret) {
 		v4l2_err(&dev->v4l2_dev, "Failed to register video device\n");
 		video_device_release(vfd);
