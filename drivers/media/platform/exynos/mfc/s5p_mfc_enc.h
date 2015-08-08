bf52a87d785a3ffdb6fd9bbf2df89e1ba3c9ec6a
Jiho Chang <jiho04.chang@samsung.com>
2015-08-08 04:16:39 +0000

    [COMMON] media: mfc: DRV2.0: add enc_internal.h and dec_internal.h
    The files defines s5p_mfc_fmt, v4l2_queryctrl, and s5p_mfc_ctrl_cfg
    for encoding and decoding, respectively.
    
    Change-Id: I7f8c07e1e446bda0033855b2d9940531ebac55b7
    Signed-off-by: Jiho Chang <jiho04.chang@samsung.com>

--- drivers/media/platform/exynos/mfc/s5p_mfc_enc.h
+++ drivers/media/platform/exynos/mfc/s5p_mfc_enc.h
@@ -13,12 +13,6 @@
 #ifndef __S5P_MFC_ENC_H
 #define __S5P_MFC_ENC_H __FILE__
 
-#define MFC_ENC_AVG_FPS_MODE
-
-#define ENC_HIGH_FPS	(70000)
-#define ENC_MAX_FPS	(120000)
-#define ENC_AVG_FRAMES	(10)
-
 #include "s5p_mfc_common.h"
 
 const struct v4l2_ioctl_ops *get_enc_v4l2_ioctl_ops(void);
