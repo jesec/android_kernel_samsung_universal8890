d23ff83cb9f6165056fa646d0776b28fd37b0f4a
Dietmar Eggemann <dietmar.eggemann@arm.com>
2015-09-01 17:30:31 +0900

    [COMMON] irq: experimental: Restrict irq_default_affinity to hmp_slow_cpu_mask
    This patch limits the default affinity mask for all IRQs to the CPUs of the A7
    cluster.
    
    Reading /proc/irq/default_smp_affinity will return the hmp_slow_cpu_mask
    instead of the full cpu mask.
    
    This patch has the positive side effect that an irq thread which has its
    IRQTF_RUNTHREAD set inside irq_thread() -> irq_wait_for_interrupt() will not
    overwrite its struct task_struct->cpus_allowed with a full cpu mask of
    desc->irq_data.affinity in irq_thread_check_affinity() essentially reverting
    patch "HMP: experimental: Force all rt tasks to start on little domain." for
    this irq thread.
    
    Change-Id: I3d9831206b625072bacddfb47ca64e4bc4fb18af
    Signed-off-by: Dietmar Eggemann <dietmar.eggemann@arm.com>

--- kernel/irq/irqdesc.c
+++ kernel/irq/irqdesc.c
@@ -24,11 +24,18 @@
  */
 static struct lock_class_key irq_desc_lock_class;
 
+#ifdef CONFIG_SCHED_HMP
+extern struct cpumask hmp_slow_cpu_mask;
+#endif
 #if defined(CONFIG_SMP)
 static void __init init_irq_default_affinity(void)
 {
 	alloc_cpumask_var(&irq_default_affinity, GFP_NOWAIT);
+#ifdef CONFIG_SCHED_HMP
+	cpumask_copy(irq_default_affinity, &hmp_slow_cpu_mask);
+#else
 	cpumask_setall(irq_default_affinity);
+#endif
 }
 #else
 static void __init init_irq_default_affinity(void)
