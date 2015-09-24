76681645d2f72df2344982a10090cf6a1af277db
Jinsung Yang <jsgood.yang@samsung.com>
2015-09-24 21:21:09 +0900

    [COMMON] ion: allow cacheable buffer allocation from CMA
    Change-Id: If88941db5e082e9dd2d6061cf59e94a4ad7d3513
    Signed-off-by: Ayoung Sim <a.sim@samsung.com>
    Signed-off-by: Jinsung Yang <jsgood.yang@samsung.com>

--- drivers/staging/android/ion/ion_cma_heap.c
+++ drivers/staging/android/ion/ion_cma_heap.c
@@ -75,9 +75,6 @@ static int ion_cma_allocate(struct ion_heap *heap, struct ion_buffer *buffer,
 
 	dev_dbg(dev, "Request buffer allocation len %ld\n", len);
 
-	if (buffer->flags & ION_FLAG_CACHED)
-		return -EINVAL;
-
 	if (align > PAGE_SIZE)
 		return -EINVAL;
 
