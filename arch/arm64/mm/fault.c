5861825443c5f7d30a68b44d571878d2a8ab92e3
Hyunki Koo <hyunki00.koo@samsung.com>
2015-10-24 19:22:25 +0900

    [COMMON] arm64: mm: implement safe panic handler
    This patch add safe panic handler to handle corrupted
    current_thread_info or current task.
    
    Change-Id: I6ad7e0c43a282871bcfae09d2082833aaff52f34
    Signed-off-by: Hyunki Koo <hyunki00.koo@samsung.com>

--- arch/arm64/mm/fault.c
+++ arch/arm64/mm/fault.c
@@ -29,6 +29,7 @@
 #include <linux/sched.h>
 #include <linux/highmem.h>
 #include <linux/perf_event.h>
+#include <linux/exynos-ss.h>
 
 #include <asm/exception.h>
 #include <asm/debug-monitors.h>
@@ -37,7 +38,9 @@
 #include <asm/pgtable.h>
 #include <asm/tlbflush.h>
 
+static int safe_fault_in_progress = 0;
 static const char *fault_name(unsigned int esr);
+extern void exynos_ss_panic_handler_safe(struct pt_regs *regs);
 
 /*
  * Dump out the page tables associated with 'addr' in mm 'mm'.
@@ -79,6 +82,19 @@ void show_pte(struct mm_struct *mm, unsigned long addr)
 	printk("\n");
 }
 
+static int __do_kernel_fault_safe(struct mm_struct *mm, unsigned long addr,
+		unsigned int esr, struct pt_regs *regs)
+{
+	safe_fault_in_progress = 0xFAFADEAD;
+
+	exynos_ss_panic_handler_safe(regs);
+	exynos_ss_printkl(safe_fault_in_progress,safe_fault_in_progress);
+	while(1)
+		wfi();
+
+	return 0;
+}
+
 /*
  * The kernel tried to access some page that wasn't present.
  */
@@ -90,6 +106,10 @@ static void __do_kernel_fault(struct mm_struct *mm, unsigned long addr,
 	 */
 	if (fixup_exception(regs))
 		return;
+	if (safe_fault_in_progress) {
+		exynos_ss_printkl(safe_fault_in_progress, safe_fault_in_progress);
+		return;
+	}
 
 	/*
 	 * No handler, we'll have to terminate things with extreme prejudice.
@@ -356,6 +376,10 @@ static int __kprobes do_translation_fault(unsigned long addr,
 					  unsigned int esr,
 					  struct pt_regs *regs)
 {
+	/* We may have invalid '*current' due to a stack overflow. */
+	if (!virt_addr_valid(current_thread_info()) || !virt_addr_valid(current))
+		__do_kernel_fault_safe(NULL, addr, esr, regs);
+
 	if (addr < TASK_SIZE)
 		return do_page_fault(addr, esr, regs);
 
