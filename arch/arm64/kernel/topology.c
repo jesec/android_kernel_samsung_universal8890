9cb8051a8bd8c51d7204594e39cc4623c34a40d9
Youngtae Lee <yt0729.lee@samsung.com>
2015-11-25 22:01:39 +0900

    [COMMON] arm64: topology: change cpu efficiency about MNGS
    Change-Id: I101e6c13e19527e97e53f0cb34e7b6c5f821aa34
    Signed-off-by: Youngtae Lee <yt0729.lee@samsung.com>

--- arch/arm64/kernel/topology.c
+++ arch/arm64/kernel/topology.c
@@ -202,7 +202,7 @@ struct cpu_efficiency {
  * use the default SCHED_POWER_SCALE value for cpu_scale.
  */
 static const struct cpu_efficiency table_efficiency[] = {
-	{ "arm,mongoose", 4000 },	/* FIXME: temporary value */
+	{ "arm,mongoose", 5035 },
 	{ "arm,cortex-a57", 3891 },
 	{ "arm,cortex-a53", 2048 },
 	{ NULL, },
