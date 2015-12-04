da5ee9632aaac4ab26cf1d20008356a1f8092520
Lee Minho <minho55.lee@samsung.com>
2015-12-04 16:06:41 +0900

    [COMMON] usb: host: xhci: add waiting for completion of hub_event
    Sometimes root hub was disconnected by remove_hcd function
    even if ports of the hub were not disconnected completely.
    And, because of root hub disconnection like above, deadlock occurred.
    To prevent the deadlock, waiting code for completion of hub_event was added.
    
    Change-Id: I0ecae45f7d3bc8781e8b9667dfb28d7593a89455
    Signed-off-by: Lee Minho <minho55.lee@samsung.com>

--- drivers/usb/host/xhci.h
+++ drivers/usb/host/xhci.h
@@ -1440,6 +1440,13 @@ static inline unsigned int hcd_index(struct usb_hcd *hcd)
 		return 1;
 }
 
+/*
+ * Sometimes deadlock occurred between hub_event and remove_hcd.
+ * In order to prevent it, waiting for completion of hub_event was added.
+ * This is a timeout (300msec) value for the waiting.
+ */
+#define XHCI_HUB_EVENT_TIMEOUT	(300)
+
 /* There is one xhci_hcd structure per controller */
 struct xhci_hcd {
 	struct usb_hcd *main_hcd;
