af7e222280a475b070140b0e22201da7d3cf1351
Jesse Chan <jc@lineageos.org>
2017-09-17 23:27:35 -0700

    Revert "sched/fair: Avoid redundant idle_cpu() call in update_sg_lb_stats()"
    This reverts commit a4c85e5d9cbf6170721469fb4f91ef65c3c5fe1d.
    
    Signed-off-by: Jesse Chan <jc@lineageos.org>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -6146,7 +6146,7 @@ static inline void update_sg_lb_stats(struct lb_env *env,
 			bool *overload)
 {
 	unsigned long load;
-	int i, nr_running;
+	int i;
 
 	memset(sgs, 0, sizeof(*sgs));
 
@@ -6163,8 +6163,7 @@ static inline void update_sg_lb_stats(struct lb_env *env,
 		sgs->group_util += cpu_util(i);
 		sgs->sum_nr_running += rq->cfs.h_nr_running;
 
-		nr_running = rq->nr_running;
-		if (nr_running > 1)
+		if (rq->nr_running > 1)
 			*overload = true;
 
 #ifdef CONFIG_NUMA_BALANCING
@@ -6172,10 +6171,7 @@ static inline void update_sg_lb_stats(struct lb_env *env,
 		sgs->nr_preferred_running += rq->nr_preferred_running;
 #endif
 		sgs->sum_weighted_load += weighted_cpuload(i);
-		/*
-		 * No need to call idle_cpu() if nr_running is not 0
-		 */
-		if (!nr_running && idle_cpu(i))
+		if (idle_cpu(i))
 			sgs->idle_cpus++;
 	}
 
