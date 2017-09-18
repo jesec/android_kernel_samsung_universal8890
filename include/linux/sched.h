cce35c3fb43202348d6c6fd2c3fdf985230092eb
Jesse Chan <jc@lineageos.org>
2017-09-17 23:12:27 -0700

    Revert "DEBUG: sched,cpufreq: add cpu_capacity change tracepoint"
    This reverts commit cadd6e99b407a0025e4e9d093622e895817330ad.

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -939,8 +939,6 @@ struct sched_group_energy {
 	struct capacity_state *cap_states; /* ptr to capacity state array */
 };
 
-unsigned long capacity_curr_of(int cpu);
-
 struct sched_group;
 
 struct sched_domain {
