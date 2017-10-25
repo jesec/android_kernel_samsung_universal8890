samsungexynos9810/kernel_google_exynos9110

pwm: samsung: Fix suspend code
The old code could ignore period_ns and duty_ns values on suspend
function when they were "0". This patch fixes it and prevent from
back-light bug when susped/resume.

Change-Id: If195f4ed77d6a71349db464cc4cfbe4ae081e39d
Signed-off-by: eunki.kim <eunki_kim@samsung.com>

drivers/pwm/pwm-samsung.c
@@ -159,6 +159,8 @@ static int s3c_pwm_config(struct pwm_chip *chip, struct pwm_device *pwm,
 	} else
 		tin_ns = NS_IN_HZ / clk_get_rate(s3c->clk);
 
+	s3c->duty_ns = duty_ns;
+
 	/* Note, counters count down */
 
 	tcmp = duty_ns / tin_ns;
@@ -298,8 +300,8 @@ static int s3c_pwm_suspend(struct device *dev)
 	 * Otherwise driver leaves PWM unconfigured if same values
 	 * passed to pwm_config
 	 */
-	s3c->period_ns = 0;
-	s3c->duty_ns = 0;
+	s3c->period_ns = -1;
+	s3c->duty_ns = -1;
 
 	return 0;
 }

jesec/android_kernel_samsung_universal8890

Signed-off-by: Changki Kim <changki.kim@samsung.com>

drivers/pwm/pwm-samsung.c
@@ -564,8 +564,8 @@ static int pwm_samsung_suspend(struct device *dev)
 		if (!chan)
 			continue;
 
-		chan->period_ns = 0;
-		chan->duty_ns = 0;
+		chan->period_ns = -1;
+		chan->duty_ns = -1;
 	}
 
 	return 0;