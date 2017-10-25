samsungexynos9810/kernel_google_exynos9110

cpufreq: compared policy's min/max when set policy
If new min_freq greater than current max_freq, policy is not
updated to new policy. But, policy shuld be update to new policy
when both updated min_freq and max_freq if max_freq greater than
min_freq.

Change-Id: Ia63f0958f5f6e2c1275b31625e3585fee2fb8d0f
Signed-off-by: Taekki Kim <taekki.kim@samsung.com>

drivers/cpufreq/cpufreq.c
@@ -1682,7 +1682,8 @@ static int __cpufreq_set_policy(struct cpufreq_policy *data,
 	memcpy(&policy->cpuinfo, &data->cpuinfo,
 				sizeof(struct cpufreq_cpuinfo));
 
-	if (policy->min > data->max || policy->max < data->min) {
+	if ((policy->min > data->max || policy->max < data->min) &&
+		(policy->max < policy->min)) {
 		ret = -EINVAL;
 		goto error_out;
 	}

jesec/android_kernel_samsung_universal8890

drivers/cpufreq/cpufreq.c
@@ -2175,7 +2175,8 @@ static int cpufreq_set_policy(struct cpufreq_policy *policy,
 
 	memcpy(&new_policy->cpuinfo, &policy->cpuinfo, sizeof(policy->cpuinfo));
 
-	if (new_policy->min > policy->max || new_policy->max < policy->min)
+	if ((new_policy->min > policy->max || new_policy->max < policy->min) &&
+		(new_policy->max < new_policy->min))
 		return -EINVAL;
 
 	/* verify the cpu speed can be set within this limit */